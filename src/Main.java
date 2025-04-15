7218845ddeacf4c137666df702ccd6fc364d4509
Notebook <230103108@sdu.edu.kz>
2025-04-15 23:36:52 +0500

    I added new Annotation

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class A{
+    @MyAnno
+    public void calculate(){
+        System.out.println("A-B");
+    }
+}
+class B extends A{
+    @Override
+    public void calculate(){
+        System.out.println("A+B");
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        int a=0;
-        System.out.println(a);
+        String s = "a b c";
+        System.out.println(s.split(" ")[0]);
     }
 }
